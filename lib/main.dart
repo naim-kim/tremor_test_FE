c1281961576f35bc56daae0a75cdd2e7550624b5
naimkim <naimk5451@gmail.com>
2025-10-20 20:36:54 +0900

    refrac: code struc

--- lib/main.dart
+++ lib/main.dart
@@ -59,8 +59,10 @@ class TremorDetectionApp extends StatelessWidget {
           ),
           textTheme: const TextTheme(
             headlineLarge: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            headlineMedium:
-                TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+            headlineMedium: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+            ),
             bodyLarge: TextStyle(fontSize: 16),
             bodyMedium: TextStyle(fontSize: 14),
           ),
